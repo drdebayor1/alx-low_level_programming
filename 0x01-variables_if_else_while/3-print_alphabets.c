d8fcc4a4a2be6a93420155240e96f82ab2e6f414
drdebator1 <aderibigbeadebayoadebayo26@gmail.com>
2022-09-12 11:24:27 -0700

    Capital alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Proramming is positive, zero, negative"
+ * Return 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar (a);
+	}
+	for (a = 'A' ; a <= 'Z' ; a++)
+	{
+		putchar (a);
+	}
+putchar('\n')
+return (0)
+}
