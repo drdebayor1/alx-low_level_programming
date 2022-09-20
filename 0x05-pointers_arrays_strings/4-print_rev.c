77a8d49389837bd70837c708d8c53b546e27df3d
drdebayor1 <111037645+drdebayor1@users.noreply.github.com>
2022-09-21 00:28:09 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,17 +10,17 @@
 
 void print_rev(char *s)
 {
-  int i;
-
-i = 0;
-while (s[i] != '\0')
+	int i;
+	
+	i = 0;
+	while (s[i] != '\0')
 {
-i++;
+		i++;
 }
 
 for (i = i - 1 ; i >= 0; i--)
 {
-_putchar(s[i]);
+	_putchar(s[i]);
 }
 
 _putchar ('\n');
