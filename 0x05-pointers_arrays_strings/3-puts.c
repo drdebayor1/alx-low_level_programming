4f8ae07147bdd32f4c61f7334a00a3d6cabce8bb
drdebayor1 <111037645+drdebayor1@users.noreply.github.com>
2022-09-21 00:22:04 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - put a string
+ *@str: A pointer to an integer that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+void _puts(char *str);
+{
+  while (*str != '\0')
+    _putchar(*str + 0);
+      str++
+  
+  _putchar('\n');
+}
