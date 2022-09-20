acd0bb3930c3a02bf5c4f99f97c7354636c7734b
drdebayor1 <111037645+drdebayor1@users.noreply.github.com>
2022-09-21 00:45:11 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - prints a reverse string
+ *@s: A pointer to an int that will be changed
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+  int l, i;
+  char ch;
+  
+  for (l = 0; s[l] != '\o'; l++)
+    
+  for (i = 0; i < l / 2; i++)
+  {
+    ch = s[l];
+    s[l] = s[l -1 - i 
+  }
+}
