b2d74797efe9367a5ce67ae120a40277663f7717
drdebator1 <aderibigbeadebayoadebayo26@gmail.com>
2022-09-20 11:08:00 -0700

    Task 1 - Reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,11 @@
 /**
  * main - check te code
  *
+ * Description: Resetvalue of n
+ *
  * Return: Always 0
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _putchar(chhar c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif //MAIN_H
+#endif MAIN_H
