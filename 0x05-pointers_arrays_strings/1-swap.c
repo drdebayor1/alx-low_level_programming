5bfbc21c55ba250a81641281f15cf46418eb2c54
drdebator1 <aderibigbeadebayoadebayo26@gmail.com>
2022-09-20 11:43:58 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,13 +5,13 @@
  * main - check the code
  *
  * Description: using the main function
- * this program prints "reset to 98"
+ * this program prints "swap integers"
  *
  * Return: Always 0
  */
 void swap_int(int *a, int *b)
 {
-  int c = *a;
-  *a = *b;
-  *b = c;
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
