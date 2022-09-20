13657c3a93f058fe944a1f8efe5e48b2a38fde0e
drdebator1 <aderibigbeadebayoadebayo26@gmail.com>
2022-09-20 11:56:11 -0700

    Counting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Description: using the main function
+ *
+ * this program prints "swap integers
+ * 
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int count;
+	for (count = 0; *s != '\0'; ++s)
+		count++;
+
+	return (count);
+}
