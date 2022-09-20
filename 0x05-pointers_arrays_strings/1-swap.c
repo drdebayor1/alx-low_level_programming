2241fbb99f9f07819bcd541fc22ce307553feb4d
drdebayor1 <111037645+drdebayor1@users.noreply.github.com>
2022-09-20 19:35:45 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Description: using the main function
+ * this program reset to 98
+ *
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+  int c = *a;
+  *a = *b;
+  *b = c;
+}
