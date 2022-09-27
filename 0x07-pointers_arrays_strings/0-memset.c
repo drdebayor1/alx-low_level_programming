642c140651462a2f35910b2e4a7cc49d9356b807
drdebator1 <aderibigbeadebayoadebayo26@gmail.com>
2022-09-26 21:30:36 -0700

    trask 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
+ *_memset - fills memory with a constant byte
+ *@s: pointer block of memory to fils
  *@b: value to set
  *@n: bytes of the memory
  *Return: dest
@@ -16,8 +16,8 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b; /*add 1 position s*/
-
-	} /*END FOR*/
+	}
+	/*END FOR*/
 
 	return (s);
 }
