14b0073cc473c55c27d92f5456a632e2a7617845
drdebayor1 <111037645+drdebayor1@users.noreply.github.com>
2022-09-28 03:38:44 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,17 +3,19 @@
  * _strpbrk - matches any character specified
  * @s: This is the C string to be scanned.
  * @accept:character in str1 that matches one of the characters in str2
- *Return: string s that matches any character specified in accept
- **/
+ *Return: strings that matches any character specified in accept
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int j;
 
-	while (*s != '\0') /*Declaring WHILE*/
+	while (*s != '\0')
+		/*Declaring WHILE*/
 	{
 		j = 0;
-		while (accept[j] != '\0')  /*Evaluating *accept*/
+		while (accept[j] != '\0')
+			/*Evaluating *accept*/
 		{
 			if (*s == accept[j])
 			{
