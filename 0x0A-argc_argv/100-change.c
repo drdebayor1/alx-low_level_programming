50cd30c585ccd5665b7163822c63a1e17ef06821
drdebator1 <aderibigbeadebayoadebayo26@gmail.com>
2022-09-30 20:19:47 -0700

    tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,28 +15,21 @@ int main(int argc, char *argv[])
 	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
 	position = total = change = aux = 0;
-
 	if (argc != 2)
-
 	{
-
 		printf("Error\n");
 
 		return (1);
 
 	}
 	total = atoi(argv[1]); /*Covert str to int*/
-
 	if (total <= 0)
 	{
-
 		printf("0\n");
 
 		return (0);
 
-	}
-	/*Declaring While*/
-
+	} /*Declaring While*/
 	while (coins[position] != '\0')
 	{
 		if (total >= coins[position])
@@ -51,7 +44,6 @@ int main(int argc, char *argv[])
 		position++;
 
 	}
-
 	printf("%d\n", change);
 
 	return (0);
