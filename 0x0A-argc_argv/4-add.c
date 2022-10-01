e843619ef0d2c2ab221e69ebfda0dfbfc76de1c3
drdebayor1 <111037645+drdebayor1@users.noreply.github.com>
2022-10-01 04:08:55 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 
 	while (count < argc) /*Goes through the whole array*/
 	{
-		if(check_num(argv[count]))
+		if (check_num(argv[count]))
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
